65bce699f46bb529ca3ec0e191c89c5b5a44a32d
Saikiran <saisuparnasa@gmail.com>
2020-07-06 16:47:52 +0530

    Loggers

---
+++ logger/src/main/java/Task_5/logger/NegativeValuesException.java
@@ -0,0 +1,10 @@
+package Task_5.logger;
+
+@SuppressWarnings("serial")
+public class NegativeValuesException extends Exception {
+	
+	NegativeValuesException(String message){
+		super(message);
+	}
+
+}
